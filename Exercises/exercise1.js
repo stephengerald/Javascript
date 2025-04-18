e11bfd814dbeba51bb44f3561579d59a7622d5ad
STERUX <stephenokpala11@gmail.com>
2025-04-18 19:44:17 +0100

    assignment

---
+++ Exercises/exercise1.js
@@ -0,0 +1,9 @@
+//Exercise 1: Even or odd Checker
+
+let number = prompt("Enter a number:");
+number = parseInt(number);
+if (number % 2 === 0) {
+    alert("This is an even number!")
+} else {
+    alert('This is an odd number!')
+}
---
+++ Exercises/exercise2.js
@@ -0,0 +1,28 @@
+//Exercise 2: Basic calculator
+
+let num1 = prompt("Enter the first number:");
+num1 = parseFloat(num1)
+let operator = prompt("Enter operator (-, +, /, *):");
+let num2 = prompt("Enter second number:");
+num2 = parseFloat(num2);
+
+let = result;
+if (operator === "+") {
+    result = num1 + num2;
+} else if (operator === "-") {
+    result = num1 - num2;
+} else if (operator === "*") {
+    result = num1 * num2;
+} else if (operator === "/") {
+    if (num2 !== 0) {
+        result = num1 / num2;
+    } else {
+        alert("Error:Division by zero is not allowed!");
+        result = "Undefined";
+    }
+} else {
+    alert("Invalid operator! Please use (-, +, *, or /)");
+    result = "Undefined";
+}
+// show result
+alert("Result: " + result);
